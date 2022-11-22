32d7b7c7540ee312b89693c3e7c3c7d7c46104f1
Mardoqueu Sousa <mdq767@gmail.com>
2022-11-22 09:16:29 -0300

    setup initial projetct

---
+++ src/main/java/com/mardoqueu/springdatamongodb/SpringDataMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.mardoqueu.springdatamongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataMongodbApplication.class, args);
+	}
+
+}
